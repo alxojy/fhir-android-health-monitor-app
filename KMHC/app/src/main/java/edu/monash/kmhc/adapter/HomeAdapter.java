06027c9d88cbc6202144683a6f52dee03bbe174a
yhew0003 <yhew0003@student.monash.edu>
2020-05-02 20:19:31 +0800

    Created Home Adapter and Home Fragment Class

--- KMHC/app/src/main/java/edu/monash/kmhc/adapter/HomeAdapter.java
+++ KMHC/app/src/main/java/edu/monash/kmhc/adapter/HomeAdapter.java
@@ -1,4 +1,73 @@
 package edu.monash.kmhc.adapter;
 
-public class HomeAdapter {
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+
+import edu.monash.kmhc.R;
+import edu.monash.kmhc.model.PatientModel;
+import edu.monash.kmhc.model.observation.ObservationModel;
+import edu.monash.kmhc.viewModel.HomeViewModel;
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder> {
+
+    private HashMap<PatientModel, ObservationModel> patientObservationHashMap ;
+
+    public HomeAdapter(HashMap<PatientModel, ObservationModel> patientObservationHashMap) {
+        this.patientObservationHashMap = patientObservationHashMap;
+        Log.i("HomeAdapter","HomeAdapter - Constructor Called");
+
+    }
+
+    @NonNull
+    @Override
+    public HomeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.i("HomeAdapter","HomeAdapter - OnCreateViewHolder Called");
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.patient_cardview,parent,false);
+        HomeViewHolder viewHolder = new HomeViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeViewHolder holder, int position) {
+        Log.i("HomeAdapter", "HomeAdapter - onBindViewHolder Called");
+        //temporary solution
+        if (patientObservationHashMap != null) {
+
+            // how to get key ???????
+
+            String cholVal = patientObservationHashMap.get(position).getValue();
+            String cholUnit = patientObservationHashMap.get(position).getUnit();
+
+            holder.cholesterolValue.setText(cholVal + " " + cholUnit);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return patientObservationHashMap.size();
+    }
+
+    public class HomeViewHolder extends  RecyclerView.ViewHolder {
+        public TextView patientName;
+        public TextView cholesterolValue;
+        public TextView time;
+        public HomeViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            patientName = itemView.findViewById(R.id.cv_patientName);
+            cholesterolValue = itemView.findViewById(R.id.cv_cholVal);
+            time = itemView.findViewById(R.id.cv_time);
+
+        }
+    }
 }
--- KMHC/app/src/main/java/edu/monash/kmhc/view/HomeFragment.java
+++ KMHC/app/src/main/java/edu/monash/kmhc/view/HomeFragment.java
@@ -10,17 +10,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
 
 import edu.monash.kmhc.R;
+import edu.monash.kmhc.adapter.HomeAdapter;
 import edu.monash.kmhc.model.PatientModel;
 import edu.monash.kmhc.model.observation.ObservationModel;
-import edu.monash.kmhc.model.observation.ObservationType;
 import edu.monash.kmhc.viewModel.HomeViewModel;
-import io.reactivex.Observable;
 
 /**
  * This fragment is used to display the main home screen upon login.
@@ -30,25 +29,38 @@ import io.reactivex.Observable;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    private TextView textView;
+    //private TextView textView;
+    private RecyclerView recyclerView;
+    private HomeAdapter homeAdapter;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        textView = root.findViewById(R.id.text_home);
 
-        homeViewModel.getAllPatientObservations().observe(getViewLifecycleOwner(), new Observer<HashMap<PatientModel, ObservationModel>>() {
+        //textView = root.findViewById(R.id.text_home);
+
+        recyclerView = root.findViewById(R.id.home_recycler_view);
+
+        homeViewModel.getPatientMutableLiveData().observe(getViewLifecycleOwner(), patientUpdatedObserver);
+
+        return root;
+    }
+
+        Observer<HashMap<PatientModel, ObservationModel>> patientUpdatedObserver = new Observer<HashMap<PatientModel, ObservationModel>>() {
             @Override
             public void onChanged(HashMap<PatientModel, ObservationModel> patientObservationHashMap) {
                 System.out.println("another");
-                patientObservationHashMap.forEach((p,o) -> {
-                    textView.setText(p.getName() + " " + o.getValue() + " " + o.getUnit());
-                });
+                homeAdapter = new HomeAdapter(patientObservationHashMap);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                homeAdapter = new HomeAdapter(patientObservationHashMap);
+                recyclerView.setAdapter(homeAdapter);
+//                patientObservationHashMap.forEach((p,o) -> {
+//                    textView.setText(p.getName() + " " + o.getValue() + " " + o.getUnit());
+//                });
             }
-        });
-            return root;
+        };
     }
 
-}
