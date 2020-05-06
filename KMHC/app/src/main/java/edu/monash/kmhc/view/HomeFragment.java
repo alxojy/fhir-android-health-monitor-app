cbc3431fade183e68f599339175370a87da59e97
yhew0003 <yhew0003@student.monash.edu>
2020-05-07 06:54:16 +0800

    Fixed HomeFragment polling bug

--- KMHC/app/src/main/java/edu/monash/kmhc/view/HomeFragment.java
+++ KMHC/app/src/main/java/edu/monash/kmhc/view/HomeFragment.java
@@ -5,69 +5,45 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.navigation.NavigationView;
-
-import java.sql.SQLOutput;
 import java.util.HashMap;
 
+import edu.monash.kmhc.MainActivity;
 import edu.monash.kmhc.R;
 import edu.monash.kmhc.adapter.HomeAdapter;
 import edu.monash.kmhc.model.PatientModel;
-import edu.monash.kmhc.viewModel.SharedViewModel2;
+import edu.monash.kmhc.viewModel.SharedViewModel;
 
 /**
  * This fragment is used to display the main home screen upon login.
  * All patients monitored by the health practitioner will be displayed in this screen.
- * TODO: Select, add, remove patient functionality.
  */
-public class HomeFragment extends Fragment implements HomeAdapter.OnPatientClickListener , NavigationView.OnNavigationItemSelectedListener{
+public class HomeFragment extends Fragment implements HomeAdapter.OnPatientClickListener {
+
     private HomeFragment thisFrag ;
-    private SharedViewModel2 sharedViewModel;
-    //private TextView textView;
+    private SharedViewModel sharedViewModel;
     private RecyclerView recyclerView;
     private HomeAdapter homeAdapter;
-
-    // for navigation drawer
-    private DrawerLayout drawer;
-    private NavigationView navigationView;
-    private ImageButton drawerToggle;
 
     Toolbar toolbar;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-
-        sharedViewModel = ViewModelProviders.of(getActivity()).get(SharedViewModel2.class);
+        sharedViewModel = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         //set tool bar
         toolbar = root.findViewById(R.id.home_toolbar);
-//        drawer = root.findViewById(R.id.home_drawer_layout);
-//        navigationView = root.findViewById(R.id.home_nav_view);
-//        drawerToggle = root.findViewById(R.id.home_menu_option);
         setUpToolBar();
-
-
-
-
-        //textView = root.findViewById(R.id.text_home);
-
 
         recyclerView = root.findViewById(R.id.home_recycler_view);
 
@@ -81,7 +57,6 @@ public class HomeFragment extends Fragment implements HomeAdapter.OnPatientClick
         Observer<HashMap<String, PatientModel>> patientUpdatedObserver = new Observer<HashMap<String, PatientModel>>() {
             @Override
             public void onChanged(HashMap<String, PatientModel> patientObservationHashMap) {
-                //System.out.println("another");
                 recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
                 homeAdapter = new HomeAdapter(patientObservationHashMap,thisFrag);
                 recyclerView.setAdapter(homeAdapter);
@@ -91,14 +66,10 @@ public class HomeFragment extends Fragment implements HomeAdapter.OnPatientClick
     @Override
     public void onPatientClick(int position, PatientModel patient) {
         // TODO: move fragment call
-        PatientInfoFragment patientInfo= new PatientInfoFragment(patient);
-        getActivity().getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, patientInfo, "patientInfoFragment")
-                .addToBackStack(null)
-                .commit();
         // navigate to new fragment
         // pass in the patient
-
+        MainActivity main = (MainActivity) getActivity();
+        main.newPatientInfoFragment(MainActivity.patient_info__fragment,patient);
     }
 
     public void setUpToolBar(){
@@ -108,40 +79,13 @@ public class HomeFragment extends Fragment implements HomeAdapter.OnPatientClick
         Toolbar.OnMenuItemClickListener menuItemClickListener = new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                // TODO : implement update server click
-                // TODO : move call to activity
-                if (item.getItemId()== R.id.menu_patient_edit){
-                    Fragment select_patient_frag = getActivity().getSupportFragmentManager().findFragmentByTag("select_patient");
-                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,select_patient_frag,"select_patient")
-                            .commit();
-                }
-
-                else if (item.getItemId() ==R.id.menu_update_server){
-                    System.out.println("update server click");
-                }
+                // ToDO: move fragment call
+                MainActivity main = (MainActivity) getActivity();
+                main.findFragment(MainActivity.select_patients_fragment);
                 return true;
             }
         };
         toolbar.setOnMenuItemClickListener(menuItemClickListener);
-    }
-
-    //swipe to delete
-    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
-        @Override
-        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-            return false;
-        }
-
-        @Override
-        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-            // check back youtube video coding with Mitch
-        }
-    };
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        drawer.closeDrawer(GravityCompat.END);
-        return false;
     }
 }
 
